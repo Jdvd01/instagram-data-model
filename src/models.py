cc46b7a8ca141bfc841fc7229819f7c69f181397
Jdvd01 <velasquezdenorajose@gmail.com>
2022-04-23 02:03:00 +0000

    entrega del proyecto

--- src/models.py
+++ src/models.py
@@ -8,26 +8,85 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+class Usuario(Base):
+    __tablename__ = "usuario"
+    id = Column(Integer, primary_key = True)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    post = relationship("post")
+    seguidos = relationship("seguidos")
+    seguidores = relationship("seguidores")
 
-    def to_dict(self):
-        return {}
+    user = Column(String(20), nullable = False, unique = True)
+    email = Column(String(40), nullable = False, unique = True)
+    password = Column(String(15), nullable = False, unique = False)
+    nombre = Column(String(20), nullable = False, unique = False)
+    apellido = Column(String(20), nullable = True)
+
+class Seguidos(Base):
+    __tablename__ = "seguidos"
+    id = Column(Integer, primary_key = True)
+
+    usuario = relationship("usuario")
+    usuario_id = Column(Integer, ForeignKey("usuario.id"))
+
+    seguido_id = Column(Integer, nullable = False, unique = True)
+
+
+class Seguidores(Base):
+    __tablename__ = "seguidores"
+    id = Column(Integer, primary_key = True)
+
+    usuario = relationship("usuario")
+    usuario_id = Column(Integer, ForeignKey("usuario.id"))
+
+    seguidore_id = Column(Integer, nullable = False, unique = True)
+
+
+
+class Post(Base):
+    __tablename__ = "post"
+    id = Column(Integer, primary_key = True)
+
+    usuario = relationship("usuario")
+    usuario_id = Column(Integer, ForeignKey("usuario.id"))
+
+    comentario = relationship("comentario")
+    comentario_id = Column(Integer, ForeignKey("comentario.id"))
+
+    me_gusta = relationship("me_gusta")
+    me_gusta_id = Column(Integer, ForeignKey("me_gusta.id"))
+
+    guardado = relationship("guardado")
+    guardado_id = Column(Integer, ForeignKey("guardado.id"))
+
+    descripcion = Column(String(160), nullable = False, unique = False)
+
+    ####################################################
+    # No estoy seguro de que tipo de dato lleva imagen #
+    ####################################################
+    imagen = Column(String(1), nullable = False, unique = False)
+
+class Comentario(Base):
+    __tablename__ = "comentario"
+    id = Column(Integer, primary_key = True)
+
+    post = relationship("post")
+
+    mensaje = Column(String(160), nullable = False, unique = False)
+    
+class MeGusta(Base):
+    __tablename__ = "me_gusta"
+    id = Column(Integer, primary_key = True)
+
+    post = relationship("post")
+    
+
+class Guardado(Base):
+    __tablename__ = "guardado"
+    id = Column(Integer, primary_key = True)
+
+    post = relationship("post")
+
 
 ## Draw from SQLAlchemy base
 try:
